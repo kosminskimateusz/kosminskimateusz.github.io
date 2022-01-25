fa82cc417e988322683d2c9c0231fe392b0c1e49
Mateusz Kosminski <kosminskimateusz@gmail.com>
2022-01-25 08:45:53 +0100

    Changed site href for current page

--- js/currentPage.js
+++ js/currentPage.js
@@ -1,13 +1,7 @@
-const url = "http://127.0.0.1:5500";
 let pages = document.getElementsByTagName("a");
 
-// console.log(url);
-
 for (let i = 0; i < pages.length; i++) {
-   if (pages[i].href === (url + location.pathname))
-   {
-      // pages[i].classList.remove("side-bar-link");
+   if (pages[i].href === (window.location.href)) {
       pages[i].classList.add("current");
-      console.log(pages[i].classList)
    }
 }
