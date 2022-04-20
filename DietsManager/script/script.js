a49b4de0085e449752b6f29e041ee4d26f7d75b2
mk <kosminskimateusz@gmail.com>
2022-04-20 15:38:58 +0200

    Diets Manager - Hidden scrollbar on sidebar

--- DietsManager/script/script.js
+++ DietsManager/script/script.js
@@ -1,41 +1,43 @@
-const THEME_BUTTON = document.querySelector("#theme-button");
-const BODY = document.querySelector("body");
+// const THEME_BUTTON = document.querySelector("#theme-button");
+// const BODY = document.querySelector("body");
 
-THEME_BUTTON.addEventListener('click', () => {
-    if (BODY.classList.contains("dark")) {
-        BODY.classList.remove("dark");
-        BODY.classList.add("light");
-    } else {
-        BODY.classList.remove("light");
-        BODY.classList.add("dark");
-    }
-})
+// THEME_BUTTON.addEventListener('click', () => {
+//     if (BODY.classList.contains("dark")) {
+//         BODY.classList.remove("dark");
+//         BODY.classList.add("light");
+//     } else {
+//         BODY.classList.remove("light");
+//         BODY.classList.add("dark");
+//     }
+// })
 
 
+import { print } from './page.js'
 let products;
 
+print();
 // await fetch('http://localhost:5000/products/')
 //     .then(res => res.json())
 //     .then(data => products = data)
 //     .then(() => console.log(products));
 
 
-async function getProducts() {
-    try {
-        let exam;
-        const response = await (await fetch('http://localhost:5000/products/')
-            .then(res => res.json())
-            .then(data => exam = data));
+// async function getProducts() {
+//     try {
+//         let exam;
+//         const response = await (await fetch('https://www.boredapi.com/api/activity')
+//             .then(res => res.json())
+//             .then(data => exam = data));
 
-        return exam;
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+//         return exam;
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
 
-products = getProducts();
+// products = getProducts();
 
-console.log(products);
+// console.log(products);
 
 const HAMBURGER = document.querySelector('.hamburger');
 const SIDE_BAR = document.querySelector('#side-bar');
